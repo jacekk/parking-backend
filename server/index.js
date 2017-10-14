230569370b6dbd1379a274d37cd6081b553b9f93
Marcin Wysocki <wysocki.marcin92@gmail.com>
2017-10-14 18:59:52 +0200

    /parkings endpoint

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('O HAI');
+/*
+    name: string,
+    freePlaces: number
+*/
+
+const fakeParkings = [
+    {
+        name: 'Renoma',
+        freePlaces: 230
+    },
+    {
+        name: 'Nowy Targ',
+        freePlaces: 30
+    },
+    {
+        name: 'Galeria Dominikańska',
+        freePlaces: 0
+    },
+]
+
+app.get('/parkings', (req, res) => {
+    res.send(fakeParkings);
 });
 
-
 app.listen('3000', () => {
-    console.log('Running on http://localhost:3000')
-})
+    console.log('Running on http://localhost:3000');
+});
