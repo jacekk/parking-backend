ad79b75cab624094d2b3bcb99c962fbec27a9075
Sebastian Rosik <sebastian.rosik@rst.com.pl>
2017-10-14 19:19:22 +0200

    Basic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">parkly</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="app-body">
+        <ul className="parkingList">
+          <li className="parkingList-item">
+            <h3 className="parking-name">Galeria Dominikańska</h3>
+            Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+          </li>
+            <li className="parkingList-item">
+              <h3 className="parking-name">Galeria Dominikańska</h3>
+              Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+            </li>
+            <li className="parkingList-item">
+              <h3 className="parking-name">Galeria Dominikańska</h3>
+              Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+            </li>
+            <li className="parkingList-item">
+              <h3 className="parking-name">Galeria Dominikańska</h3>
+              Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+            </li>
+            <li className="parkingList-item">
+              <h3 className="parking-name">Galeria Dominikańska</h3>
+              Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+            </li>
+            <li className="parkingList-item">
+              <h3 className="parking-name">Galeria Dominikańska</h3>
+              Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+            </li>
+            <li className="parkingList-item">
+              <h3 className="parking-name">Galeria Dominikańska</h3>
+              Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+            </li>
+            <li className="parkingList-item">
+              <h3 className="parking-name">Galeria Dominikańska</h3>
+              Wolnych miejsc: <strong className="parking-spot--good">32</strong>
+            </li>
+          </ul>
+        </main>
       </div>
     );
   }
