05a8a5ef8dc9a9e129cc777ccd5602bb21ffdc54
jacekk <github.com@jacek-k.net>
2017-10-14 19:20:56 +0200

    added csv fetching

--- server/data-fetch/run.js
+++ server/data-fetch/run.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env nodejs
 
+const getCSV = require('get-csv');
 const C = require('./constants');
 
+const fetchError = (err) => {
+    console.error('Fetch error', err);
+}
 
+const fetchSuccess = (rows) => {
+    const withoutHeaders = rows.slice(1);
+
+    console.log('number of rows', withoutHeaders.length);
+};
+    
 const run = () => {
-    console.log(C);
+    getCSV(C.CSV_FILE_URL, { headers: false })
+        .then(fetchSuccess)
+        .catch(fetchError)
+    ;
 };
 
 run();
