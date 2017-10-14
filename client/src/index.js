66c206aa20c527826371e14ea1cafde8490328f0
Marcin Wysocki <wysocki.marcin92@gmail.com>
2017-10-14 19:32:54 +0200

    fetching parkings

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const SERVER_URL = 'http://localhost:3000';
+const PARKINGS_ROUTE = 'parkings';
+
+const getAllParkings = () =>
+    axios
+        .get(`${SERVER_URL}/${PARKINGS_ROUTE}`)
+        .then(resp => resp.data)
+        .catch(console.error);
+
+ReactDOM.render(<App getParkings={getAllParkings}/>, document.getElementById('root'));
 registerServiceWorker();
