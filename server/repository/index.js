19775413a8bdc15781c91721b19d05c72db2e5c5
Amadeusz Winogrodzki <winogrodzki@Amadeuszs-MBP.home>
2017-10-14 22:37:36 +0200

    changed getParkings method

--- server/repository/index.js
+++ server/repository/index.js
@@ -62,15 +62,7 @@ const getRepository = () => getCollections.then(({ locationCollection, entriesCo
       });
     }),
     getParkings: () => new Promise((resolve, reject) => {
-        entriesCollection.aggregate([
-            { $sort: { time: -1 } },
-            { $group: { 
-                _id: '$name', 
-                freeSpots: { $first: '$freeSpots' },
-                id: { $first: '$locationId' }
-            }},
-            { $project: { name: "$_id", _id: 0, freeSpots: 1, id: 1 } }
-        ])
+        locationCollection.find()
         .toArray((err, data) => {
             if (err) {
                 reject(err);
