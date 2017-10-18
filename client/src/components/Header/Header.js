19251d2f31f0108a51283e1819bfa1c874fa318a
Sebastian Rosik <sebastian.rosik@rst.com.pl>
2017-10-18 08:38:37 +0200

    Changed header style

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 const Header = ({ backButtonVisible, backButtonHandler }) => (
-    <header className="app-header">
-        { backButtonVisible && <button className="app-back" onClick={backButtonHandler}>Powrót</button> }
-        <h1 className="app-title">parkly</h1>
+    <header className="header">
+        { backButtonVisible && <button className="header-backButton" onClick={backButtonHandler}>Powrót</button> }
+        <h1 className="header-title">parkly</h1>
     </header>
 );
+
 
 export default Header;
