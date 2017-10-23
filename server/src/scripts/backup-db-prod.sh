16c9f3d17ede7904abfcb0431cb01311061891a6
jacekk <github.com@jacek-k.net>
2017-10-23 22:45:46 +0200

    added another backup script for prod db;

--- server/src/scripts/backup-db-prod.sh
+++ server/src/scripts/backup-db-prod.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 NOW_FORMATTED=`date +%Y%m%d-%H%M%S-%Z`
-BACKUP_DIR="./db-backups"
+BACKUP_DIR="/backups/dbs"
 BACKUP_PATH="$BACKUP_DIR/$NOW_FORMATTED"
 
 mkdir -p $BACKUP_PATH
 
 mongodump \
     --db parkly \
-    --host mongodb \
+    --host locahost \
     --out $BACKUP_PATH
 
 echo "Backed up sucessfully into: $BACKUP_PATH."
